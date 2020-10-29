3fe95cf6dd823a072b41afd5f8187eb46426f5bb
MariyaLcs <mariya.podosinova@gmail.com>
2020-10-29 15:24:21 +0000

    HomeScreen Product Listing

---
+++ shop-client/src/products.js
@@ -0,0 +1,109 @@
+const products = [
+  {
+    _id: "1",
+    name: "Evening dress Black and White",
+    image: "/images/1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus deserunt in ut quae veniam, aspernatur itaque cum porro velit sed.",
+    brand: "X&Y",
+    category: "Dress",
+    price: 40,
+    countInStock: 2,
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    _id: "2",
+    name: "Evening dress Blue",
+    image: "/images/2.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus deserunt in ut quae veniam, aspernatur itaque cum porro velit sed.",
+    brand: "X&Y",
+    category: "Dress",
+    price: 40,
+    countInStock: 2,
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    _id: "3",
+    name: "Evening dress Pink",
+    image: "/images/3.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus deserunt in ut quae veniam, aspernatur itaque cum porro velit sed.",
+    brand: "X&Y",
+    category: "Dress",
+    price: 40,
+    countInStock: 2,
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    _id: "4",
+    name: "Evening dress Gray",
+    image: "/images/4.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus deserunt in ut quae veniam, aspernatur itaque cum porro velit sed.",
+    brand: "X&Y",
+    category: "Dress",
+    price: 40,
+    countInStock: 2,
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    _id: "5",
+    name: "Evening dress Rose",
+    image: "/images/5.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus deserunt in ut quae veniam, aspernatur itaque cum porro velit sed.",
+    brand: "X&Y",
+    category: "Dress",
+    price: 40,
+    countInStock: 2,
+    rating: 4.5,
+    numReviews: 10,
+  },
+
+  {
+    _id: "6",
+    name: "Evening dress Navy Blue",
+    image: "/images/6.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus deserunt in ut quae veniam, aspernatur itaque cum porro velit sed.",
+    brand: "X&Y",
+    category: "Dress",
+    price: 40,
+    countInStock: 2,
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    _id: "7",
+    name: "Evening dress Black and Pink",
+    image: "/images/7.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus deserunt in ut quae veniam, aspernatur itaque cum porro velit sed.",
+    brand: "X&Y",
+    category: "Dress",
+    price: 40,
+    countInStock: 2,
+    rating: 4.5,
+    numReviews: 10,
+  },
+  {
+    _id: "8",
+    name: "Evening dress White",
+    image: "/images/8.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus deserunt in ut quae veniam, aspernatur itaque cum porro velit sed.",
+    brand: "X&Y",
+    category: "Dress",
+    price: 40,
+    countInStock: 2,
+    rating: 4.5,
+    numReviews: 10,
+  },
+];
+
+export default products;
