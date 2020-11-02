9f2038fbf784a91756765743b549ed0664f1538b
MariyaLcs <mariya.podosinova@gmail.com>
2020-11-02 14:14:33 +0000

    adding Custom Error handling

--- shop-server/routes/productRoutes.js
+++ shop-server/routes/productRoutes.js
@@ -17,14 +17,18 @@ router.get(
 //@desc Fetch single product
 //@route GET /api/products/:id
 //@access Public
-router.get("/:id", asyncHandler(async  (req, res) => {
-  const product = await Product.findById(req.params.id)
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
 
-    if(product){res.json(product);} else{
-        res.status(404).json({message: "Product not found"})
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product not found");
     }
-
-  
-}));
+  })
+);
 
 export default router;
