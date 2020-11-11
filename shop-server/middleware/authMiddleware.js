1811b2db707f92a7722a215ae0f28c399fb3539c
MariyaLcs <mariya.podosinova@gmail.com>
2020-11-11 22:02:13 +0000

    adding custom Authentication Middleware part3

--- shop-server/middleware/authMiddleware.js
+++ shop-server/middleware/authMiddleware.js
@@ -1,10 +1,24 @@
-import jwt from "jsonwebtoken"
-import User from "../models/userModel.js"
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
-const protect = async(req, res, next) =>{
-    let token
-    console.log(req.header.authorization);
-    next()
-}
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      console.log(decoded);
+      next();
+    } catch (error) {}
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
 
-export {protect}
+export { protect };
