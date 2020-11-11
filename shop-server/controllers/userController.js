a233c775c528f8cb851148dc7d05822aa0999817
MariyaLcs <mariya.podosinova@gmail.com>
2020-11-11 22:10:30 +0000

    adding custom Authentication Middleware part4

--- shop-server/controllers/userController.js
+++ shop-server/controllers/userController.js
@@ -27,7 +27,19 @@ const authUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/profile
 //@access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-   res.send("Success");
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 export { authUser, getUserProfile };
