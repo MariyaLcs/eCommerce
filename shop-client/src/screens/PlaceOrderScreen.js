4eefea235d5d0668b81abd553b5281a9f1f1e2ef
MariyaLcs <mariya.podosinova@gmail.com>
2020-11-20 17:27:57 +0000

    creating Order, front part2

--- shop-client/src/screens/PlaceOrderScreen.js
+++ shop-client/src/screens/PlaceOrderScreen.js
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import CheckoutSteps from "../components/CheckoutSteps";
+import { createOrder } from "../actions/orderActions";
 
-const PlaceOrderScreen = () => {
+const PlaceOrderScreen = ({history}) => {
+  const dispatch = useDispatch();
+
   const cart = useSelector((state) => state.cart);
   const addDecimals = (num) => {
     return (Math.round(num * 100) / 100).toFixed(2);
@@ -17,13 +20,34 @@ const PlaceOrderScreen = () => {
   );
   cart.shippingPrice = addDecimals(cart.itemsPrice > 20 ? 10 : 0);
   cart.taxPrice = addDecimals(Number((0.12 * cart.itemsPrice).toFixed(2)));
-  cart.totalPrice =
-    (Number(cart.itemsPrice) +
+  cart.totalPrice = (
+    Number(cart.itemsPrice) +
     Number(cart.shippingPrice) +
-    Number(cart.taxPrice)).toFixed(2);
+    Number(cart.taxPrice)
+  ).toFixed(2);
+
+  const orderCreate = useSelector((state) => state.orderCreate);
+  const { order, success, error } = orderCreate;
+
+  useEffect(()=>{
+    if(success){
+      history.pushState(`/order/${order._id}`)
+    }
+    // eslint-disable-next-line
+  },[history, success])
 
   const placeOrderHandler = () => {
-    console.log("order");
+    dispatch(
+      createOrder({
+        orderItems: cart.cartItems,
+        shippingAddress: cart.shippingAddress,
+        paymentMethod: cart.paymentMethod,
+        itemsPrice: cart.itemsPrice,
+        shippingPrice: cart.shippingPrice,
+        taxPrice: cart.taxPrice,
+        totalPrice: cart.totalPrice,
+      })
+    );
   };
 
   return (
@@ -107,6 +131,7 @@ const PlaceOrderScreen = () => {
                   <Col>£{cart.totalPrice}</Col>
                 </Row>
               </ListGroup.Item>
+                  <ListGroup.Item>{error && <Message variant="danger">{error}</Message>}</ListGroup.Item>
               <ListGroup.Item>
                 <Button
                   type="button"
