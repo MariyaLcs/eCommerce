0dfb1f246b77b8e1eee3d04e002ea89947a189d9
MariyaLcs <mariya.podosinova@gmail.com>
2020-11-30 20:18:03 +0000

    adding Admin User List

--- shop-client/src/App.js
+++ shop-client/src/App.js
@@ -13,6 +13,8 @@ import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
+import UserListScreen from "./screens/UserListScreen";
+
 
 const App = () => {
   return (
@@ -20,15 +22,16 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-        <Route path="/order/:id" component={OrderScreen}/>
-        <Route path="/shipping" component={ShippingScreen}/>
-        <Route path="/payment" component={PaymentScreen}/>
-        <Route path="/placeorder" component={PlaceOrderScreen}/>
-        <Route path="/login" component={LoginScreen}/>
-        <Route path="/register" component={RegisterScreen}/>
-        <Route path="/profile" component={ProfileScreen}/>
+          <Route path="/order/:id" component={OrderScreen}/>
+          <Route path="/shipping" component={ShippingScreen}/>
+          <Route path="/payment" component={PaymentScreen}/>
+          <Route path="/placeorder" component={PlaceOrderScreen}/>
+          <Route path="/login" component={LoginScreen}/>
+          <Route path="/register" component={RegisterScreen}/>
+          <Route path="/profile" component={ProfileScreen}/>
           <Route path="/product/:id" component={ProductScreen}/>
           <Route path="/cart/:id?" component={CartScreen}/>
+          <Route path="/admin/userlist" component={UserListScreen}/>
           <Route path="/" component={HomeScreen} exact/>
         </Container>
       </main>
