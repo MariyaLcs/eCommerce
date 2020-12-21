4ad7f93ce3bd85df817cba615b6f53f6c47b6aed
MariyaLcs <mariya.podosinova@gmail.com>
2020-12-21 16:15:46 +0000

    adding res for errors

--- shop-client/src/screens/ProductEditScreen.js
+++ shop-client/src/screens/ProductEditScreen.js
@@ -53,27 +53,27 @@ const ProductEditScreen = ({ match, history }) => {
   }, [dispatch, history, productId, product, successUpdate]);
 
   const uploadFileHandler = async (e) => {
-    const file = e.target.files[0]
-    const formData = new FormData()
-    formData.append('image', file)
-    setUploading(true)
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("image", file);
+    setUploading(true);
 
     try {
       const config = {
         headers: {
-          'Content-Type': 'multipart/form-data',
+          "Content-Type": "multipart/form-data",
         },
-      }
+      };
 
-      const { data } = await axios.post('/api/upload', formData, config)
+      const { data } = await axios.post("/api/upload", formData, config);
 
-      setImage(data)
-      setUploading(false)
+      setImage(data);
+      setUploading(false);
     } catch (error) {
-      console.error(error)
-      setUploading(false)
+      console.error(error);
+      setUploading(false);
     }
-  }
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -127,17 +127,17 @@ const ProductEditScreen = ({ match, history }) => {
               ></Form.Control>
             </Form.Group>
 
-            <Form.Group controlId='image'>
+            <Form.Group controlId="image">
               <Form.Label>Image</Form.Label>
               <Form.Control
-                type='text'
-                placeholder='Enter image url'
+                type="text"
+                placeholder="Enter image url"
                 value={image}
                 onChange={(e) => setImage(e.target.value)}
               ></Form.Control>
               <Form.File
-                id='image-file'
-                label='Choose File'
+                id="image-file"
+                label="Choose File"
                 custom
                 onChange={uploadFileHandler}
               ></Form.File>
