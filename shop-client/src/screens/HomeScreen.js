c804d86b68d4e2fc0e449674521bac830415028f
MariyaLcs <mariya.podosinova@gmail.com>
2021-01-15 11:04:27 +0000

    add Product Search functionality

--- shop-client/src/screens/HomeScreen.js
+++ shop-client/src/screens/HomeScreen.js
@@ -6,21 +6,23 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 import { listProducts } from "../actions/productActions";
 
-const HomeScreen = () => {
+const HomeScreen = ({ match }) => {
+  const keyword = match.params.keyword;
+
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
 
   useEffect(() => {
-    dispatch(listProducts());
-  }, [dispatch]);
+    dispatch(listProducts(keyword));
+  }, [dispatch, keyword]);
 
   return (
     <>
       <h1>Our Showroom</h1>
       {loading ? (
-        <Loader/>
+        <Loader />
       ) : error ? (
         <Message variant="danger">{error}</Message>
       ) : (
